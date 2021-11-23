a41cf6a85d91a2dd1bc12d3e8f599f8abc2d9949
Rizzy256 <85558980+Rizzy256@users.noreply.github.com>
2021-11-23 06:08:36 -0800

    Add files via upload

---
+++ Do while loop(Even numbers).cpp
@@ -0,0 +1,22 @@
+//136636 Gabriel Katumba
+//Assignment 2
+#include <iostream>
+using namespace std;
+
+int main() {
+	int i = 11;
+	do{
+  
+      if(i % 2 == 0){
+   
+         cout <<i<< " ";
+      }
+  
+  
+     i++;
+	}
+	
+while(i <= 50);
+
+	return 0;	
+}
