a53bd58250c570e88031d55799892faad45b89a8
Rizzy256 <85558980+Rizzy256@users.noreply.github.com>
2021-11-23 06:04:07 -0800

    Add files via upload

---
+++ while loop(out put numbers).cpp
@@ -0,0 +1,22 @@
+//Gabriel Katumba 136636
+//Assignment 1
+#include <iostream>
+using namespace std;
+
+int main() {
+ 
+    int i=11;
+ 
+   while( i <= 28){
+  
+      if(i % 2 == 0){
+   
+         cout <<i<< " ";
+      }
+  
+  
+     i++;
+ }
+ 
+ return 0;
+}
