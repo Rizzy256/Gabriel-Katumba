8ce976020afe2b6c933b475e5e240d87ecb8bc08
Rizzy256 <85558980+Rizzy256@users.noreply.github.com>
2021-12-29 01:21:07 -0800

    Add files via upload

---
+++ Schedule project 136636.cpp
@@ -0,0 +1,98 @@
+#include<iostream>
+#include<string>
+using namespace std;
+
+int main(){
+	int x;
+	cout<<"1.Monday\n";
+	cout<<"2.Tuesday\n";
+	cout<<"3.Wednesday\n";
+	cout<<"4.Thursday\n";
+	cout<<"5.Friday\n";
+	cout<<"6.Saturday\n";
+	cout<<"7.Sunday\n";
+	
+	cout<<"Enter a date of the week:";
+	cin>>x;
+	switch(x){
+		case 1:
+			cout<<"Monday\n";
+			cout<< "\n"
+			cout << "1. Prince Komla : Manages sales\n";
+			cout << "2. Gabriel Katumba : Receives orders\n";
+			cout << "3. Monica Barenzi : Serves drinks\n";
+			cout << "4. Conrad Kabuye : Serves Meals\n";
+			cout << "5. Reis Bashaijja : Welcomes Customers\n";
+			cout << "6. Sean Mike : Washes dishes\n";
+			cout << "7. Chef Ramsey : Main Chef\n";
+			cout << "8. Kelvin Kakuru : Assistant Chef\n";
+			break;
+		case 2:
+			cout<<"Tuesday\n";
+			cout << "1. Prince Komla : Receives orders\n";
+			cout << "2. Gabriel Katumba : Manages sales\n";
+			cout << "3. Monica Barenzi : Serves meals\n";
+			cout << "4. Conrad Kabuye : Serves drinks\n";
+			cout << "5. Reis Bashaijja : Washes dishes\n";
+			cout << "6. Sean Mike : Welcomes customers\n";
+			cout << "7. Chef Ramsey : Main Chef\n";
+			cout << "8. Kelvin Kakuru : Assistant Chef\n";
+			break;
+		case 3:
+		   	cout<<"Wednesday\n";
+		   	cout << "1. Prince Komla : Manages sales\n";
+			cout << "2. Gabriel Katumba : Receives orders\n";
+			cout << "3. Monica Barenzi : Serves drinks\n";
+			cout << "4. Conrad Kabuye : Serves Meals\n";
+			cout << "5. Reis Bashaijja : Welcomes Customers\n";
+			cout << "6. Sean Mike : Washes dishes\n";
+			cout << "7. Chef Ramsey : Main Chef\n";
+			cout << "8. Kelvin Kakuru : Assistant Chef\n";
+			break;
+		case 4:
+			cout<<"Thursday\n";
+			cout << "1. Prince Komla : Manages sales\n";
+			cout << "2. Gabriel Katumba : Receives orders\n";
+			cout << "3. Monica Barenzi : Serves drinks\n";
+			cout << "4. Conrad Kabuye : Serves Meals\n";
+			cout << "5. Reis Bashaijja : Welcomes Customers\n";
+			cout << "6. Sean Mike : Washes dishes\n";
+			cout << "7. Chef Ramsey : Main Chef\n";
+			cout << "8. Kelvin Kakuru : Assistant Chef\n";
+			break;
+		case 5:
+			cout<<"Friday\n";
+			cout << "1. Prince Komla : Manages sales\n";
+			cout << "2. Gabriel Katumba : Receives orders\n";
+			cout << "3. Monica Barenzi : Serves drinks\n";
+			cout << "4. Conrad Kabuye : Serves Meals\n";
+			cout << "5. Reis Bashaijja : Welcomes Customers\n";
+			cout << "6. Sean Mike : Washes dishes\n";
+			cout << "7. Chef Ramsey : Main Chef\n";
+			cout << "8. Kelvin Kakuru : Assistant Chef\n";
+			break;
+		case 6:
+			cout<<"Saturday\n";
+			cout << "1. Prince Komla : Manages sales\n";
+			cout << "2. Gabriel Katumba : Receives orders\n";
+			cout << "3. Monica Barenzi : Serves drinks\n";
+			cout << "4. Conrad Kabuye : Serves Meals\n";
+			cout << "5. Reis Bashaijja : Welcomes Customers\n";
+			cout << "6. Sean Mike : Washes dishes\n";
+			cout << "7. Chef Ramsey : Main Chef\n";
+			cout << "8. Kelvin Kakuru : Assistant Chef\n";
+			break;	
+		case 7:
+			cout << "Sunday\n";
+			cout << "1. Prince Komla : Manages sales\n";
+			cout << "2. Gabriel Katumba : Receives orders\n";
+			cout << "3. Monica Barenzi : Serves drinks\n";
+			cout << "4. Conrad Kabuye : Serves Meals\n";
+			cout << "5. Reis Bashaijja : Welcomes Customers\n";
+			cout << "6. Sean Mike : Washes dishes\n";
+			cout << "7. Chef Ramsey : Main Chef\n";
+			cout << "8. Kelvin Kakuru : Assistant Chef\n";
+			break;
+	}
+return 0;
+}
